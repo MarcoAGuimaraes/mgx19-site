664ba24d521a4043e7881b3e9dff11f688ec6dcb
Marco Guimaraes <magu1908a@gmail.com>
2026-05-02 00:35:27 -0300

    Melhorias responsivas, ajustes mobile e menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("MGX19 site carregado");
+document.addEventListener("DOMContentLoaded", () => {
+
+  const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+
+  console.log("toggle:", toggle); // debug
+
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+  }
+
+});
