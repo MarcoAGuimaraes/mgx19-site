5a95a93e46fb8f25743e84191e2906b88031562a
Marco Guimaraes <magu1908a@gmail.com>
2026-05-02 15:05:00 -0300

    formulario funcionando com emailjs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // MENU
   const toggle = document.querySelector(".menu-toggle");
   const menu = document.querySelector(".menu");
-
   console.log("toggle:", toggle); // debug
 
   if (toggle && menu) {
@@ -10,5 +10,25 @@ document.addEventListener("DOMContentLoaded", () => {
       menu.classList.toggle("active");
     });
   }
+
+  // EMAILJS INIT
+  emailjs.init("ln3paXVg4VoRXCS0U");
+
+  // FORM
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_9k04ynr", "template_z2piglm", this)
+      .then(() => {
+        alert("Mensagem enviada com sucesso!");
+        form.reset();
+      }, (error) => {
+        alert("Erro ao enviar. Tente novamente.");
+        console.log(error);
+      });
+
+  });
 
 });
